8fa771a2a30d2cdbeb899956c7a47439f3768c26
prosper edafe <mamusedafe19@gmail.com>
2023-01-20 15:39:56 +0100

    build

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
+import Spinner from "./spinner";
+
 function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
-      axios.get(url).then((response) => {
-        setData(response.data);
-        console.log(response.data);
-      });
+      axios.get(url)
+        .then((response) => {
+          setData(response.data);
+          console.log(response.data);
+          setLoading(true)
+        });
       setLocation("");
     }
+    //   .catch ((err) => {
+    //   console.log(err);
+    //   // setLoading(false);
+    // });
   };
 
   return (
@@ -28,6 +37,9 @@ function App() {
           type="text"
         />
       </div>
+      {loading && (
+        <Spinner />
+      )}
       <div className="container">
         <div className="top">
           <div className="location">
